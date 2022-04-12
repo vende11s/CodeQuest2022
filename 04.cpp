e855367c2a029519b722e2c5196d4212ee0071f4
vende11s <74505994+vende11s@users.noreply.github.com>
2022-04-12 12:43:49 +0200

    shit

---
+++ 04.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int in(){
+int inX;
+cin>>inX;
+return inX;
+}
+
+int main()
+{
+    int z(in());
+    while(z--){
+        string number; cin>>number;
+        string s; cin>>s;
+
+        if(s=="PARENTHESES"){
+            number.insert(0,"(");
+            number.insert(4,")");
+            number.insert(5," ");
+            number.insert(9,"-");
+        }
+        if(s=="DASHES"){
+            number.insert(3,"-");
+            number.insert(7,"-");
+        }
+        if(s=="PERIODS"){
+            number.insert(3,".");
+            number.insert(7,".");
+        }
+
+
+        cout<<number<<endl;
+
+    }
+}
